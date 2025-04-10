e96534181ffd4350f58b51f1c5bbfeda12b09ae2
Rafael Santos Costa <rafaelscosta71@gmail.com>
2025-04-09 21:25:51 -0300

    Finalização da Aula 04.B.ac Expressões JOIN e Views.sql

--- PTBPABD/Aula 04.B.ac Expressões JOIN e Views.sql
+++ PTBPABD/Aula 04.B.ac Expressões JOIN e Views.sql
@@ -1,14 +1,79 @@
+/*Exercicio 1*/
 select i.id, i.name, count(teaches.sec_id) as Number_Of_Sections
 from instructor i  
-join teaches on i.id = teaches.id
+full outer join teaches on i.id = teaches.id
 group by i.id, i.name;
 
-select * from instructor;
-select * from section where sec_id = 1;
-select * from takes;
-select * from prereq p ;
-select * from classroom c;
-SELECT * FROM ADVISOR;
-select * from course;
-select * from department d ;
-select * from teaches;
+
+/*Exercicio 2*/
+select i.id, i.name,(
+select count(t.sec_id) from teaches t where t.id = i.id) as Number_Of_Sections
+from instructor i;
+
+/*Exercicio 3*/
+select s.course_id, s.sec_id, s.semester, s.year, i.id, coalesce(i.name, '-') AS instructor_name
+from section s
+left join teaches t on s.course_id = t.course_id 
+                   and s.sec_id = t.sec_id 
+                   and s.semester = t.semester 
+                   and s.year = t.year
+left join instructor i on t.id and s.year = 2010;
+
+/*Exercicio 4*/
+select stu.id, stu.name, c.title, stu.dept_name, t.grade, 
+case
+	when t.grade = 'A+' then 4
+	when t.grade = 'A' then 3.7
+	when t.grade = 'A-' then 3.4
+	when t.grade = 'B+' then 3.1
+	when t.grade = 'B' then 2.8
+	when t.grade = 'B-' then 2.5
+	when t.grade = 'C+' then 2.2
+	when t.grade = 'C' then 1.9
+	when t.grade = 'C-' then 1.6
+	else null
+end as points, c.credits, 
+case
+	when t.grade = 'A+' then 4
+	when t.grade = 'A' then 3.7
+	when t.grade = 'A-' then 3.4
+	when t.grade = 'B+' then 3.1
+	when t.grade = 'B' then 2.8
+	when t.grade = 'B-' then 2.5
+	when t.grade = 'C+' then 2.2
+	when t.grade = 'C' then 1.9
+	when t.grade = 'C-' then 1.6
+	else null
+end * c.credits as Pontos_Totais from student as stu
+join takes as t on stu.id = t.id
+join course as c on t.course_id = c.course_id;
+
+/*Exercicio 5*/
+create view Coeficiente_Rendimento as
+select stu.id, stu.name, c.title, stu.dept_name, t.grade, 
+case
+	when t.grade = 'A+' then 4
+	when t.grade = 'A' then 3.7
+	when t.grade = 'A-' then 3.4
+	when t.grade = 'B+' then 3.1
+	when t.grade = 'B' then 2.8
+	when t.grade = 'B-' then 2.5
+	when t.grade = 'C+' then 2.2
+	when t.grade = 'C' then 1.9
+	when t.grade = 'C-' then 1.6
+	else null
+end as points, c.credits, 
+case
+	when t.grade = 'A+' then 4
+	when t.grade = 'A' then 3.7
+	when t.grade = 'A-' then 3.4
+	when t.grade = 'B+' then 3.1
+	when t.grade = 'B' then 2.8
+	when t.grade = 'B-' then 2.5
+	when t.grade = 'C+' then 2.2
+	when t.grade = 'C' then 1.9
+	when t.grade = 'C-' then 1.6
+	else null
+end * c.credits as Pontos_Totais from student as stu
+join takes as t on stu.id = t.id
+join course as c on t.course_id = c.course_id;
