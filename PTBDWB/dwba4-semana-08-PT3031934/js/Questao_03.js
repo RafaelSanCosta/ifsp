23f658bf1b936f493c45c357fd79cff9a5bfb339
RafaelSanCosta <rafaelscosta71@gmail.com>
2025-05-20 14:59:40 -0300

    exercicio da semana 8 web

---
+++ PTBDWB/dwba4-semana-08-PT3031934/js/Questao_03.js
@@ -0,0 +1,29 @@
+/*Aluno 1*/ 
+var nomeAluno = "João da Silva";
+var primeiraNota = 7;
+var segundaNota = 8.5;
+var notaTotal = (primeiraNota * 0.6) + (segundaNota * 0.4);
+
+alert("Aluno: " + nomeAluno + "\nPrimeira Nota: "
+    +primeiraNota+"\nSegunda Nota: " + segundaNota);
+alert("Nota total do aluno: "+notaTotal.toPrecision(2));
+
+/*Aluno 2*/ 
+var nomeAluno = "Papagaio Uruguaio de Assunção";
+var primeiraNota = 8.5;
+var segundaNota = 9.75;
+var notaTotal = (primeiraNota * 0.6) + (segundaNota * 0.4);
+
+alert("Aluno: " + nomeAluno + "\nPrimeira Nota: "
+    +primeiraNota+"\nSegunda Nota: " + segundaNota);
+alert("Nota total do aluno: "+notaTotal.toPrecision(2));
+
+/*Aluno 3*/ 
+var nomeAluno = "Beltrano II";
+var primeiraNota = 4;
+var segundaNota = 10;
+var notaTotal = (primeiraNota * 0.6) + (segundaNota * 0.4);
+
+alert("Aluno: " + nomeAluno + "\nPrimeira Nota: "
+    +primeiraNota+"\nSegunda Nota: " + segundaNota);
+alert("Nota total do aluno: "+notaTotal.toPrecision(2));
---
+++ PTBDWB/dwba4-semana-08-PT3031934/js/Questao_04.js
@@ -0,0 +1,58 @@
+class Aluno{
+    constructor(primeiroNome, segundoNome, primeiraNota, segundaNota)
+    {
+    this.primeiroNome = primeiroNome;
+    this.segundoNome = segundoNome;
+    this.primeiraNota = primeiraNota;
+    this.segundaNota = segundaNota;
+    }
+
+    nomeCompleto(){
+        return this.primeiroNome + " " + this.segundoNome;
+    }
+
+    media(){
+        return ((this.primeiraNota * 0.6) + (this.segundaNota * 0.4)).toPrecision(2);
+    }
+
+    situacao(){
+        if(this.media()>6){
+            return "Aprovado"
+        } else {
+            return "Reprovado"
+        }
+    }
+}
+
+const aluno1 = new Aluno("Light", "Yagami", 10.0, 9.8);
+const aluno2 = new Aluno("Naruto", "Uzumaki", 6.0, 6.5); 
+const aluno3 = new Aluno("Sasuke", "Uchiha", 8.5, 9.0); 
+const aluno4 = new Aluno("Edward", "Elric", 9.5, 9.2);  
+const aluno5 = new Aluno("Monkey D.", "Luffy", 5.0, 4.5);    
+
+let alunos = [aluno1, aluno2, aluno3, aluno4, aluno5];
+
+/*
+let text = "<ul>";
+alunos.forEach(listaDeAlunos);
+text += "</ul>";
+
+document.getElementById("demo").innerHTML = text;
+
+function listaDeAlunos(value) {
+  text += "<li>" + value.toString() + "</li>";
+} 
+*/
+//vi isso no w3schools e n funcionou
+
+let text = "<p>";
+
+for(var i=0; i<alunos.length;i++){
+    text += alunos[i].nomeCompleto() + 
+    " | Media: " + alunos[i].media() + 
+    " | Situação: " + alunos[i].situacao() + 
+    "<br>";
+}
+
+text += "</p>";
+document.getElementById("demo").innerHTML = text;
