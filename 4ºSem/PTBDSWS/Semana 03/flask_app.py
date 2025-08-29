b589fcdffd3769489eef708cf10c5f055c81f511
Rafael Santos Costa <rafaelscosta71@gmail.com>
2025-08-28 23:33:40 -0300

    Create flask_app.py

---
+++ 4ºSem/PTBDSWS/Semana 03/flask_app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template #request, abort, url_for,
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<username>')
+def user(username):
+    return render_template('user.html', name=username)
+
+@app.route('/rotainexistente')
+def rotaNaoExistente():
+    return render_template('404.html'),404
+
+@app.errorhandler(404)
+def pagina_nao_encontrada(e):
+    return render_template('404.html'),404
+
+if __name__ == "__main__":
+    app.run(debug=True)
